e9d0a763f279587ffbbc68a045ebe3ef261488df
Ameen <ameen@campus-102-207.ucdavis.edu>
2016-10-25 21:42:13 -0700

    SparseBlockToDenseLinear completed, seems to work

--- SparseBlockToDenseLinear.lua
+++ SparseBlockToDenseLinear.lua
@@ -63,7 +63,7 @@ function SparseBlockToDenseLinear:pri_getSubWeight(i)
 	return self:pri_getSubW(i, self.weight)
 end
 
-function SparseBlockToDenseLinear:pri_getSubGradWieght(i)
+function SparseBlockToDenseLinear:pri_getSubGradWeight(i)
 	return self:pri_getSubW(i, self.gradWeight)
 end
 
@@ -79,28 +79,31 @@ function SparseBlockToDenseLinear:pri_ensureOutput(input)
 end
 
 function SparseBlockToDenseLinear:pri_updateOutput_column(taInput, teWeight)
-	self.outputBufferA:zero()
-	self.outputBufferB:zero()
+--	self.outputBufferA:zero() -- no need to reset all to zero, only what's used here
+--	self.outputBufferB:zero() -- ToDo: possible optimization: instead of this, can scatter zero scalar to what's non-zero at the end
 
 	-- calculate the output for non-Sparse blocks
 	local teInput = taInput.teValue
 	local nRows = teInput:size(1)
 	local teOutput = self.outputBufferA:narrow(1, 1, nRows)
+	teOutput:zero()
 	teOutput:addmm(teInput, teWeight)
 	
 	-- copy result to buffer
-	print(taInput.teRowIdx)
 	local teDstIdx = torch.expand(taInput.teRowIdx, nRows, self.nOutputWidth)
-	print(teDstIdx)
 	self.outputBufferB:scatter(1, teDstIdx, teOutput)
 
 	-- add buffer to output
 	self.output:add(self.outputBufferB)
+
+	-- cleanup the buffer
+	self.outputBufferB:scatter(1, teDstIdx, 0)
 end
 
 function SparseBlockToDenseLinear:updateOutput(input)
 	self:pri_ensureWeight(input)
 	self:pri_ensureOutput(input)
+	self.output:zero()
 
 	local nColumns = table.getn(input.taData)
 	for i=1, nColumns do
@@ -109,4 +112,77 @@ function SparseBlockToDenseLinear:updateOutput(input)
 	end
 
 	return self.output
+end
+
+function SparseBlockToDenseLinear:pri_ensureGradInput(input)
+	if self.gradInput ~= nil then
+		return
+	end
+
+	self.gradInput = { nBatchSize = input.nBatchSize, taData = {} }
+
+	local nColumns = table.getn(input.taData)
+	for i=1, nColumns do
+		local taInputCurr = input.taData[i]
+
+		taGradInputCurr = { teValue = torch.zeros(taInputCurr.teValue:size()),
+												teRowIdx = taInputCurr.teRowIdx }
+		table.insert(self.gradInput.taData, taGradInputCurr)
+	end
+end
+
+function SparseBlockToDenseLinear:pri_updateGradInput_column(taInput, teGradOutput, taGradInput, teWeight)
+
+	-- copy teGradOutput to teGradOutputSelected based on teRowIdx
+	local nRows = taInput.teValue:size(1)
+	local teGradOutputSelected = self.outputBufferA:narrow(1, 1, nRows)
+	local teDstIdx = torch.expand(taInput.teRowIdx, nRows, self.nOutputWidth)
+	print(teDstIdx)
+	teGradOutputSelected:gather(teGradOutput, 1, teDstIdx)
+
+	-- calculate and update gradInput
+	local gradInput = taGradInput.teValue
+	gradInput:zero()
+	gradInput:addmm(teGradOutputSelected, teWeight:t())
+
+	-- cleanup teGradOutputSelected
+	teGradOutputSelected:zero()
+end
+
+function SparseBlockToDenseLinear:updateGradInput(input, gradOutput)
+	self:pri_ensureGradInput(input)
+
+	local nColumns = table.getn(self.gradInput.taData)
+	for i=1, nColumns do
+		self:pri_updateGradInput_column(input.taData[i],
+																		gradOutput,
+																		self.gradInput.taData[i],
+																		self:pri_getSubWeight(i))
+	end
+
+	return self.gradInput
+end
+
+function SparseBlockToDenseLinear:pri_accGradWeight_column(taInput, teGradOutput, teGradWeight, scale)
+	-- copy teGradOutput to teGradOutputSelected based on teRowIdx
+	local nRows = taInput.teValue:size(1)
+	local teGradOutputSelected = self.outputBufferA:narrow(1, 1, nRows)
+	local teDstIdx = torch.expand(taInput.teRowIdx, nRows, self.nOutputWidth)
+	teGradOutputSelected:gather(teGradOutput, 1, teDstIdx)
+
+  teGradWeight:t():addmm(scale, teGradOutputSelected:t(), taInput.teValue)
+
+	-- cleanup teGradOutputSelected
+	teGradOutputSelected:zero()
+end
+
+function SparseBlockToDenseLinear:accGradParameters(input, gradOutput, scale)
+   scale = scale or 1
+	local nColumns = table.getn(input.taData)
+	for i=1, nColumns do
+		self:pri_accGradWeight_column(input.taData[i],
+																	gradOutput,
+																	self:pri_getSubGradWeight(i), scale)
+	end
+
 end
