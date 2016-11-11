ffb0e65d301bd46220b2d6391ab3373a35f87afb
Ameen Eetemadi <eetemadi@ucdavis.edu>
2016-11-10 22:13:11 -0800

    updated getAUC with f1 score

--- gatherResults.sh
+++ gatherResults.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+nTop=$1
 for protFile in $( ls sparseData2/protInfo_expr*.csv ); do
 #	echo - $protFile -:
 	cat $protFile.desc
 	echo -n ";"
-	python3.5 ../../getAUC.py $protFile
+	python3.5 ../../getAUC.py $protFile $nTop
 done
--- getAUC.py
+++ getAUC.py
@@ -2,18 +2,42 @@ import numpy as np
 import sys
 from numpy import genfromtxt
 from sklearn import metrics
+#from operator import itemgetter
 
 strFilename = 'sparseData2/protInfo.csv'
 if len(sys.argv) > 1:
     strFilename = sys.argv[1]
 
 my_data = genfromtxt(strFilename, delimiter=',')
+
+# calucate AUC(roc)
 y = my_data[:, 2]
 pred = my_data[:, 1]
 fpr, tpr, thresholds = metrics.roc_curve(y, pred, pos_label=1)
-#print("AUC(roc):{:f}".format( metrics.auc(fpr, tpr)), end=";")
 print("{:f}".format( metrics.auc(fpr, tpr)), end=";")
 
+# calucate AUC(pr)
 precision, recall, thresholds = metrics.precision_recall_curve(y, pred, pos_label=1)
-#print("AUC(pr):{:f}".format( metrics.auc(recall, precision)))
-print("{:f}".format( metrics.auc(recall, precision)))
+print("{:f}".format( metrics.auc(recall, precision)), end=";")
+
+
+# calculate f1 score
+if len(sys.argv) > 2:
+    my_data = my_data[np.argsort(my_data[:, 1])][::-1]
+    nTop = int(sys.argv[2])
+    nRows = my_data.shape[0]
+    labels = np.zeros([nRows, 1])
+    labels[0:nTop,:].fill(1)
+    my_data = np.append(my_data, labels, 1)
+
+    # positive:
+    f1Score = metrics.f1_score(my_data[:, 2], my_data[:, 3], pos_label=1)
+    print("{:f}".format( f1Score), end=";")
+
+    # negative:
+    f1Score = metrics.f1_score(my_data[:, 2], my_data[:, 3], pos_label=0)
+    print("{:f}".format( f1Score), end=";")
+
+print()
+
+
